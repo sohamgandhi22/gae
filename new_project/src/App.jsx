a0ce57b098676c7301fc16c7158d8ca47bf7a792
Soham <sohamgandhi2210@gmail.com>
2025-01-23 19:32:52 +0530

    Initialize React project with Vite and Tailwind CSS setup

---
+++ new_project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+function App() {
+  return (
+    <>
+      <div className='bg-blue-500 text-white text-center p-4'>
+        <h1>Hello World</h1>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
